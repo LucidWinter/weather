4a29e2668f333afc9dc1e965209125ac64542c92
LucidWinter <mike.brown11311@gmail.com>
2025-09-29 14:00:31 -0400

    Weather app

---
+++ src/components/SearchBar.jsx
@@ -0,0 +1,31 @@
+import React, { useState } from "react";
+
+export default function SearchBar({ fetchWeather }) {
+  const [city, setCity] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (city.trim()) {
+      fetchWeather(city);
+      setCity("");
+    }
+  };
+
+  return (
+    <form className="flex" onSubmit={handleSubmit}>
+      <input
+        type="text"
+        placeholder="City"
+        value={city}
+        onChange={(e) => setCity(e.target.value)}
+        className="p-2 border border-gray-300 rounded-l-lg outline-none flex-1 border-r-0"
+      />
+      <button
+        className="bg-blue-500 border cursor-pointer p-2 hover:bg-blue-600 border-l-0 rounded-r-lg"
+        type="submit"
+      >
+        Search
+      </button>
+    </form>
+  );
+}
